5d0ca4cdb7cafe446aa886b7bea8771f3dbc0cce
Ethan-Duarte <dshinghisenpai@gmail.com>
2026-07-30 04:42:37 -0400

    img change

--- MaptapClone/maptapclone.js
+++ MaptapClone/maptapclone.js
@@ -4,7 +4,7 @@ import * as THREE from 'three';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-import bgImage from './charlie hebdo.jpg';
+import bgImage from './BLEH.jpg';
 
 import earthImage from './earth.jpg';
 
