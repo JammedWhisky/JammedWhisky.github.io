463017af03b24e6ee31730e1c34d8403cf328a8f
Ethan Duarte <153022400+JammedWhisky@users.noreply.github.com>
2026-09-13 21:28:16 +0000

    viteconfigfix

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,9 @@ export default defineConfig({
       input: {
         home: resolve(__dirname, 'index.html'),
         maptap: resolve(__dirname, 'MaptapClone/index.html'),
+        about: resolve(__dirname, 'about/index.html')
+        projects: resolve(__dirname, 'projects/index.html')
+        misc: resolve(__dirname, 'about/misc.html')
       },
     },
   },
